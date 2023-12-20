4108296a117c37a9fd5b86058b73cf716c6ca70f
jackson <jacksonjjacobs@gmail.com>
2023-12-19 19:09:21 -0500

    Refactoring and filter added
    Changes made:
    1. Refactored codebase
    2. Refreshed dependencies
    3. Added filtering option
    4. Fixed parcoords formatting

--- static/js/slickgrid.js
+++ static/js/slickgrid.js
@@ -1,3 +1,8 @@
+const DATA_VIEW = new Slick.Data.DataView();
+var ORIGINAL_DATASET = [];
+let SEARCH_STRING = "";
+
+
 $(document).ready(function () {
 	console.log("[LOG] Document ready.")
 	// console.log($("#brushing").attr("fn"))
@@ -6,47 +11,93 @@ $(document).ready(function () {
 	$.ajax({
 		url: "/tsv",
 		type: "GET",
-		success: function (data) {			
-			renderLines(d3.tsvParse(data));
+		success: function (data) {
+			ORIGINAL_DATASET = d3.tsvParse(data)
+			renderLines();
 		}
 	});
 	
 });
 
 
-function renderLines(lines) {
-	var parcoords = ParCoords()("#example")
-		.alpha(0.4)
-		.mode("queue") // progressive rendering
-		// .height(d3.max([document.body.clientHeight / 2, 220]))
-		.margin({
-			top: 36,
-			left: 0,
-			right: 0,
-			bottom: 16
-		});
-
-	const data = lines;
-	// slickgrid needs each data element to have an id
-	data.forEach(function (d, i) { 
+function renderLines() {
+	ORIGINAL_DATASET.forEach(function (d, i) { 
 		d.id = d.id || i; 
 		const url = window.location.origin + "/image/" + d.filename;
 		d.img = `<a href=${url}><img src=${url} style='height:100%'></a>`
 	});
 
+	const parcoords = initParcoords(ORIGINAL_DATASET);
+
+	const column_keys = d3.keys(ORIGINAL_DATASET[0]);
+	
+	const { grid, pager } = initSlickGrid(parcoords, column_keys, ORIGINAL_DATASET);
+
+
+	initDataView(grid);
+	updateFilter();
+
+	// fill grid with data
+	gridUpdate(ORIGINAL_DATASET);
+
+
+	// update grid on brush
+
+
+}
+
+function initDataView(slickgrid) {
+	function myFilter(item, args) {
+		if (args.searchString != "" && item["patientID"].indexOf(args.searchString) == -1) {
+		  	return false;
+		}
+		return true;
+	}
+
+	DATA_VIEW.onRowCountChanged.subscribe(function (e, args) {
+		slickgrid.updateRowCount();
+		slickgrid.render();
+
+	});
+
+	DATA_VIEW.onRowsChanged.subscribe(function (e, args) {
+		slickgrid.invalidateRows(args.rows);
+		slickgrid.render();
+	});
+
+	DATA_VIEW.setFilter(myFilter)
+}
+
+function initParcoords(data) {
+	const num_columns = d3.keys(data[0]).length;
+
+	var parcoords = ParCoords()("#example")
+	.alpha(0.4)
+	.mode("queue") // progressive rendering
+	// .height(d3.max([document.body.clientHeight / 2, 220]))
+	.width(100 * num_columns)
+	.margin({
+		top: 36,
+		left: 0,
+		right: 0,
+		bottom: 16
+	});
+
 	parcoords
 		.data(data)
-		.hideAxis(["filename", "gid", "img"])
+		.hideAxis(["filename", "gid", "img", "patientID"])
 		.render()
 		.reorderable()
 		.brushMode("1D-axes");
 	
-	// setting up grid
-	var column_keys = d3.keys(data[0]);
+	parcoords.on("brush", function (d) {
+		gridUpdate(d);
+	});
+	return parcoords;
+}
 
-	function formatter(row, cell, value, columnDef, dataContext) {
-        return value;
-    }
+function initSlickGrid(parcoords, column_keys, data) {
+	// setting up grid
 
 	var columns = column_keys.map(function (key, i) {
 		if (key == "img") {
@@ -77,36 +128,50 @@ function renderLines(lines) {
 	columns.unshift(imgColumn);
 
 	var options = {
+		columnPicker: {
+			columnTitle: "Columns",
+			hideForceFitButton: false,
+			hideSyncResizeButton: false,
+			forceFitTitle: "Force fit columns",
+			syncResizeTitle: "Synchronous resize",
+		},
 		enableCellNavigation: true,
 		enableColumnReorder: false,
 		multiColumnSort: false,
-		forceFitColumns: true,
+		forceFitColumns: false,
 		rowHeight: 100
 	};
 
-	var dataView = new Slick.Data.DataView();
-	var grid = new Slick.Grid("#grid", dataView, columns, options);
-	var pager = new Slick.Controls.Pager(dataView, grid, $("#pager"));
+	var grid = new Slick.Grid("#grid", DATA_VIEW, columns, options);
+	var pager = new Slick.Controls.Pager(DATA_VIEW, grid, $("#pager"));
+	var columnpicker = new Slick.Controls.ColumnPicker(columns, grid, options);
 
-	// wire up model events to drive the grid
-	dataView.onRowCountChanged.subscribe(function (e, args) {
-		grid.updateRowCount();
-		grid.render();
-	});
+	// initialize the search panel
+	let topPanel = grid.getTopPanel();
+	const topPanelLeftElm = document.querySelector("#inlineFilterPanel");
+	topPanel.appendChild(topPanelLeftElm);
+	topPanelLeftElm.style.display = 'block';
 
-	dataView.onRowsChanged.subscribe(function (e, args) {
-		grid.invalidateRows(args.rows);
-		grid.render();
+	// wire up #filter-toggle to toggle the search panel
+	$("#filter-toggle").on("click", toggleFilterRow);
+
+	// add event listener to txtSearch
+	$("#txtSearch").keyup(function (e) {
+		gridUpdate(ORIGINAL_DATASET);
+		// clear on Esc
+		if (e.which == 27) {
+			this.value = "";
+			
+
+		}
+		SEARCH_STRING = this.value;
+		updateFilter();
+		updateParcoords(parcoords, DATA_VIEW.getFilteredItems());
 	});
 
 	// column sorting
 	var sortcol = column_keys[0];
 	var sortdir = 1;
-
-	function comparer(a, b) {
-		var x = a[sortcol], y = b[sortcol];
-		return (x == y ? 0 : (x > y ? 1 : -1));
-	}
 
 	// click header to sort grid column
 	grid.onSort.subscribe(function (e, args) {
@@ -114,15 +179,14 @@ function renderLines(lines) {
 		sortcol = args.sortCol.field;
 
 		if ($.browser.msie && $.browser.version <= 8) {
-			dataView.fastSort(sortcol, args.sortAsc);
+			DATA_VIEW.fastSort(sortcol, args.sortAsc);
 		} else {
-			dataView.sort(comparer, args.sortAsc);
+			DATA_VIEW.sort(comparer, args.sortAsc);
 		}
 	});
 
 	// highlight row in chart
 	grid.onMouseEnter.subscribe(function (e, args) {
-		// Get row number from grid
 		var grid_row = grid.getCellFromEvent(e).row;
 
 		// Get the id of the item referenced in grid_row
@@ -140,22 +204,41 @@ function renderLines(lines) {
 		parcoords.unhighlight();
 	});
 
-	// fill grid with data
-	gridUpdate(data);
 
-	// update grid on brush
-	parcoords.on("brush", function (d) {
-		gridUpdate(d);
-		
-		// TODO image gallary update
+	// helper functions
+	function comparer(a, b) {
+		var x = a[sortcol], y = b[sortcol];
+		return (x == y ? 0 : (x > y ? 1 : -1));
+	}
 
+	function formatter(row, cell, value, columnDef, dataContext) {
+		return value;
+	}
+
+	function toggleFilterRow() {
+		grid.setTopPanelVisibility(!grid.getOptions().showTopPanel);
+	}
+
+	return {grid, pager};
+}
+
+function gridUpdate(data) {
+	DATA_VIEW.beginUpdate();
+	DATA_VIEW.setItems(data);
+	DATA_VIEW.endUpdate();
+};
+
+function updateFilter() {
+	DATA_VIEW.setFilterArgs({
+		searchString: SEARCH_STRING
 	});
-	
+	DATA_VIEW.refresh();
+}
 
-	function gridUpdate(data) {
-		dataView.beginUpdate();
-		dataView.setItems(data);
-		dataView.endUpdate();
-	};
-
+function updateParcoords(parcoords, data) {
+	parcoords.brushReset();
+	parcoords.brushed(false)
+	parcoords
+		.data(data)
+		.render();
 }
